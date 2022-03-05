19cb8c330070baaebc256625aa9ac3509c292205
lucas-aguero <lucasaguero@gmail.com>
2022-03-04 22:00:18 -0300

    Restart Repo

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-
-var itemsAcordeon = document.getElementsByClassName("container__acordeon__item");
-
-for (item of itemsAcordeon) {
-  item.addEventListener("click", function () {
-    this.classList.toggle("container__acordeon__item--show");
-  });
-}
